8c785fe88698ffeac851ef63a66ec4b8a72f1807
Philipp Berger <philipp.berger@nexenio.com>
2021-04-27 15:57:20 +0200

    fix: allow larger body size

--- services/backend/src/app.js
+++ services/backend/src/app.js
@@ -74,7 +74,7 @@ const configureApp = database => {
     app.use(helmet.hsts());
   }
   app.use(cookieParser());
-  app.use(express.json());
+  app.use(express.json({ limit: '500kb' }));
   app.use(express.urlencoded({ extended: false }));
   app.use(
     session({
